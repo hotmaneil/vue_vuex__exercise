d4a0a05252e83ab9394bd444f9f7d251020d6cce
Neil.Tsai <Neil.Tsai@sunstige.com>
2019-12-29 11:54:50 +0800

    送交目前進度：Vuex 中的 Getters 及 mapGetters, mapActions

---
+++ src/store/index.js
@@ -0,0 +1,101 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  strict: true,
+  state: {
+    isLoading: false,
+    products: [],
+    categories: [],
+    cart: {
+      carts: [],
+    },
+  },
+  actions: {
+    // payload 載荷
+    updateLoading(context, status) {
+      context.commit('LOADING', status);
+    },
+
+    getCart(context) {
+      context.commit('LOADING', true);
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
+      axios.get(url).then((response) => {
+        if (response.data.data.carts) {
+          context.commit('CART', response.data.data);
+        }
+        context.commit('LOADING', false);
+        console.log('取得購物車', response.data.data);
+      });
+    },
+
+    removeCart(context, id) {
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart/${id}`;
+      context.commit('LOADING', true);
+      axios.delete(url).then((response) => {
+        context.commit('LOADING', false);
+        context.dispatch('getCart');
+        console.log('刪除購物車項目', response);
+      });
+    },
+
+    addtoCart(context,  {id, qty} ) {
+      console.log(context,  id, qty );
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
+      context.commit('LOADING', true);
+      const item = {
+        product_id: id,
+        qty,
+      };
+      axios.post(url, { data: item }).then((response) => {
+        context.commit('LOADING', false);
+        context.dispatch('getCart');
+        console.log('加入購物車:', response);
+      });
+    },
+
+    getProducts(context) {
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products/all`;
+      context.commit('LOADING', true);
+      axios.get(url).then((response) => {
+        context.commit('PRODUCTS', response.data.products);
+        context.commit('CATEGORIES', response.data.products);
+        console.log('取得產品列表:', response);
+        context.commit('LOADING', false);
+      });
+    },
+  },
+
+  mutations: {
+    LOADING(state, status) {
+      state.isLoading = status;
+    },
+    PRODUCTS(state, payload) {
+      state.products = payload;
+    },
+    CATEGORIES(state, payload) {
+      const categories = new Set();
+      payload.forEach((item) => {
+        categories.add(item.category);
+      });
+      console.log("種類",categories);
+      state.categories = Array.from(categories);
+    },
+    CART(state, payload) {
+      state.cart = payload;
+    },
+    getters:{
+      // categories(state){
+      //   return state.categories;
+      // }
+      categories: state => state.categories,
+      products: state => state.products,
+      isLoading: state => state.isLoading,
+      cart: state => state.cart,
+    }
+  },
+
+});
